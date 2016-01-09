e8e4f5f8ddc66c0adebc8f7aa6efb5cd4e7770cf
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2016-01-09 14:52:38 -0500

    set up linter

--- src/main.js
+++ src/main.js
@@ -1,41 +1,41 @@
 import syllable from 'syllable';
-//var wordNet = require('wordnet-magic');
+// var wordNet = require('wordnet-magic');
 // var wn = wordNet('insert path', preload);
 
-Array.prototype.sum = function() {return this.reduce((a, b) => a + b); }
-
-export function isHaiku(msg, separator = '//') {
-	let rows = msg.split(separator).map((str) => str.trim());
-
-	var haikuLength = rows.map(countSyllables).sum();
-	if (haikuLength > 17) {
-		// what to do if commit is longer than a haiku
-	} else if ( haikuLength === 17 ) {
-		//just print the haiku ??
-	} else {
-		// what to do if commit is shorter than a haiku
-	}
-
-	return haikuLength === 17;
+Array.prototype.sum = function() {
+  return this.reduce((a, b) => a + b);
 };
 
-let countSyllables = function ( phrase ) {
-	let tokens = phrase.split(/\W/);
-	return tokens.map(syllable).sum();
-}
+let countSyllables = function(phrase) {
+  let tokens = phrase.split(/\W/);
+  return tokens.map(syllable).sum();
+};
 
+export function isHaiku(msg, separator = '//') {
+  let rows = msg.split(separator).map(str => str.trim());
+
+  var haikuLength = rows.map(countSyllables).sum();
+  if (haikuLength > 17) {
+    // what to do if commit is longer than a haiku
+  } else if (haikuLength === 17) {
+    // just print the haiku ??
+  } else {
+    // what to do if commit is shorter than a haiku
+  }
+
+  return haikuLength === 17;
+}
 
 /*
 steps to check if it is a haiku
  1. check syllable length of commit
  2. if longer - > check for haikus in commit..
- 	a) if haiku -> print done !
- 	b) if not -> get most of haiku and continue? choose key words and continue?
+  a) if haiku -> print done !
+  b) if not -> get most of haiku and continue? choose key words and continue?
  3. if shorter -> check syllable counts?
- 	for what is missing -> fill in rhyme etc. to make up length ?? nonsensical ?? 
+  for what is missing -> fill in rhyme etc. to make up length ?? nonsensical ??
 */
 
-
 export function makeHaiku(msg) {
-	return msg + ' is a haiku';
+  return msg + ' is a haiku';
 }
--- src/test.js
+++ src/test.js
@@ -1,27 +1,25 @@
 import {isHaiku} from './main.js';
 
-
 function all(arr) {
-	return arr.reduce((a, b) => a && b, true);
+  return arr.reduce((a, b) => a && b, true);
 }
 
 function any(arr) {
-	return arr.reduce((a, b) => a || b, false);
+  return arr.reduce((a, b) => a || b, false);
 }
 
-
 var haikus = [
-	'added packages // hopefully they will not suck // oh hey i can count'
+  'added packages // hopefully they will not suck // oh hey i can count'
 ];
 
 var notHaikus = [
-	'sup // my // dude!'
-]
+  'sup // my // dude!'
+];
 
 if (all(haikus.map(isHaiku))) {
-	console.log('haikus are haikus');
+  console.log('haikus are haikus');
 }
 
 if (any(notHaikus.map(isHaiku))) {
-	console.log('thought some not-haikus were haikus');
+  console.log('thought some not-haikus were haikus');
 }
