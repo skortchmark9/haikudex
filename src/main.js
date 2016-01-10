9846ccac80fd8c49f9486ba1e4019c47440906df
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2016-01-10 14:42:39 -0500

    This weekend the // codex hackathon happened // and we made a really incorrigible pale buggy thing

--- src/main.js
+++ src/main.js
@@ -9,9 +9,6 @@ import nouns from './data/nouns.js';
 import thesaurus from './data/thesaurus.js';
 
 
-
-
-
 let adjectives = descriptors.map(word => [word, syllable(word)]);
 let curses = cusseth.map(word => [word, syllable(word)]);
 
