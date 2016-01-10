5f3daf5199ec337be904df52c078be41453eb58c
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2016-01-10 14:24:01 -0500

    added new feature // for my convertible // incorrigible

--- src/main.js
+++ src/main.js
@@ -264,6 +264,7 @@ function adjustSyllables(lines, adjustment) {
         // lineAdjustment += lineCount;
       } else if (adjustment > 0) {
         lineAdjustment = adjustment > maxLineAdjustment ? maxLineAdjustment : adjustment;
+        console.log(267, lineAdjustment);
         lineAdjustment -= lineCount;
         console.log("ADJUSTMENTS: ", maxLineAdjustment, lineCount, lineAdjustment);
       }
